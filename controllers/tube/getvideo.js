82e513310ea371f7abb7ad00b5824abaa9d75ffb
CodeSandbox <support@codesandbox.io>
2026-03-06 00:06:17 +0000

    update

--- controllers/tube/getvideo.js
+++ controllers/tube/getvideo.js
@@ -127,7 +127,7 @@ router.get("/:id", async (req, res) => {
     console.time("getChannel");
     const channelData = await getChannel((isCollaborating ? Info.basic_info?.channel?.id : Info.secondary_info.owner?.author?.id) || Info.basic_info?.channel?.id || Info.secondary_info?.owner?.author?.endpoint?.payload?.panelLoadingStrategy?.inlineContent?.dialogViewModel?.customContent?.listViewModel?.listItems?.[0]?.listItemViewModel?.title?.commandRuns?.[0]?.onTap?.innertubeCommand?.browseEndpoint?.browseId || "");
     console.timeEnd("getChannel");
-    //fs.writeFileSync(JPath, JSON.stringify(Info.secondary_info, null, 2));
+    fs.writeFileSync(JPath, JSON.stringify(Info.secondary_info.description.runs, null, 2));
     const videoInfo = {
       title: Info.primary_info.title.text || "",
       channelId: (isCollaborating ? Info.basic_info?.channel?.id : Info.secondary_info.owner?.author?.id) || Info.basic_info?.channel?.id || Info.secondary_info?.owner?.author?.endpoint?.payload?.panelLoadingStrategy?.inlineContent?.dialogViewModel?.customContent?.listViewModel?.listItems?.[0]?.listItemViewModel?.title?.commandRuns?.[0]?.onTap?.innertubeCommand?.browseEndpoint?.browseId || "error",
@@ -148,7 +148,7 @@ router.get("/:id", async (req, res) => {
       watch_next_feed: watchNext || "",
     };
     //console.log(`Info.watch_next_feed: ${Info.watch_next_feed}`)
-    fs.writeFileSync(JPath, videoInfo.description);
+    //fs.writeFileSync(JPath, videoInfo.description);
     const pl = playlistId != null ? true: false;
     console.time("render");
     res.render("tube/watch.ejs", { videoData, videoInfo, videoId, baseUrl, pl });
