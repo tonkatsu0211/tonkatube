1954cc213fcf46ec7c0a9a9fdb3776583e073f55
tonkatsu0211 <tonkatsu-0211@outlook.jp>
2026-05-21 01:24:58 +0000

    update

--- controllers/tube/back.js
+++ controllers/tube/back.js
@@ -186,7 +186,7 @@ router.get("/comment/:id", async (req, res) => {
   const id = req.params.id;
   try {
     const cm = await getComments(id);
-    //fs.writeFileSync(JPath, JSON.stringify(cm, null, 2));
+    fs.writeFileSync(JPath, JSON.stringify(cm, null, 2));
     res.render("tube/back/comment", { cm });
   } catch (error) {
     console.error("comment error:", error);
