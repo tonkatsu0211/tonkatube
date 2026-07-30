7d90347e0b7211c3cb8048fa26e5abc0685ca291
tonkatsu0211 <tonkatsu-0211@outlook.jp>
2026-07-30 10:34:06 +0900

    Update youtube.js

--- server/youtube.js
+++ server/youtube.js
@@ -82,7 +82,10 @@ async function getComments(videoId) {
     if (i < 9) {
       await new Promise(resolve => setTimeout(resolve, 2000));
     }
-    if ((i + 1) % 2 == 0) await setClient();
+    if ((i + 1) % 2 == 0) {
+      console.log("recreate client");
+      await setClient();
+    }
   }
   console.error("コメント取得失敗:", lastError);
   throw new Error(`コメント取得失敗: ${lastError}`);
