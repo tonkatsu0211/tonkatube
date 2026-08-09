c6c3c4da817f32e1669c2a3d6190eabf54cb1c85
tonkatsu0211 <tonkatsu-0211@outlook.jp>
2026-08-09 22:35:24 +0900

    Update youtube.js

--- server/youtube.js
+++ server/youtube.js
@@ -145,7 +145,7 @@ async function getComments(videoId) {
         authorIsChannelOwner: false
       };
     });
-    console.dir(JSON.stringify(responce, null, 2), { depth: null });
+    console.dir(response, { depth: null });
     return { contents };
   } catch (err) {
     console.error("YouTube Data APIコメント取得失敗:", err);
