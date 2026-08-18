980a6551dae6fae4612b6e4d6bd33ebd0476ac95
tonkatsu0211 <tonkatsu-0211@outlook.jp>
2026-08-18 21:06:34 +0900

    Update server.js

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.use(
     },
   })
 );
-
+ 
 app.use((req, res, next) => {
   if (!req.get("host").includes("vercel") && !req.get("host").includes("railway")) return res.redirect(`https://tonkatube-2.vercel.app${req.url}`);
   next();
